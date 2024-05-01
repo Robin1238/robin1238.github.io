d7e73de838b79024a8c8d555982e5a24b92fd398
Robin1238 <robin1238soypro@gmail.com>
2024-05-01 13:45:02 -0500

    Version 1.0.2.1 bugs fixes

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 //Ramdon Phrase
-let phrases = [
-  'Me encanta One Piece',
-  'Gatos & Bugs. OpenAi 2024',
-  'Kaizoku-ou ni ore wa naru!!',
-  'Si abandonas ambos, eres normal.',
-  'Si eres alma, eres incomprendido.',
-  'Si eres cerebro, eres inteligente.',
-  'Sé que algún día me pagarán por hacer esto.',
-  'Arreglar el bug, enpsar cmoo bug, bug del ser.',
-  'Si recargas la página, leerás un poco de mi pensamientos.',
-  'Espero que se renderice una frase seria en un momento serio.',
-]
-const randomText = document.getElementById("random-text");
+const renderPhrase = () => {
+  let phrases = [
+    'Me encanta One Piece',
+    'Gatos & Bugs. OpenAi 2024',
+    'Kaizoku-ou ni ore wa naru!!',
+    'Si abandonas ambos, eres normal.',
+    'Si eres alma, eres incomprendido.',
+    'Si eres cerebro, eres inteligente.',
+    'Sé que algún día me pagarán por hacer esto.',
+    'Arreglar el bug, enpsar cmoo bug, bug del ser.',
+    'Si recargas la página, leerás un poco de mi pensamientos.',
+    'Espero que se renderice una frase seria en un momento serio.',
+  ]
+  const randomText = document.getElementById("random-text");
 
-let random = Math.floor(Math.random() * phrases.length)
-let randomTextPhrase = phrases[random]
+  let random = Math.floor(Math.random() * phrases.length)
+  let randomTextPhrase = phrases[random]
 
-randomText.textContent = randomTextPhrase
+  randomText.textContent = randomTextPhrase
+}
+
+renderPhrase()
 
 
 //Project pagination
